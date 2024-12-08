f2032d44217addea60dfc40337fae95604dc9e9c
Grey Lee <sieungrey.lee@mail.utoronto.ca>
2024-12-08 08:43:08 -0500

    remove reverse

--- 2024/d8/part1.py
+++ 2024/d8/part1.py
@@ -20,11 +20,10 @@ for frequency in antennas:
     others = antennas[frequency][:idx] + antennas[frequency][idx + 1:]
     for other in others:
       to_other = (other[0] - location[0], other[1] - location[1])
-      reverse_to_other = (-1 * to_other[0], -1 * to_other[1])
       target_a = (other[0] + to_other[0], other[1] + to_other[1])
-      target_b = (location[0] + reverse_to_other[0], location[1] + reverse_to_other[1])
       if (is_in_grid(target_a)):
         antinodes[target_a] = True
+      target_b = (location[0] - to_other[0], location[1] - to_other[1])
       if (is_in_grid(target_b)):
         antinodes[target_b] = True
 
--- 2024/d8/part2.py
+++ 2024/d8/part2.py
@@ -20,14 +20,13 @@ for frequency in antennas:
     others = antennas[frequency][:idx] + antennas[frequency][idx + 1:]
     for other in others:
       to_other = (other[0] - location[0], other[1] - location[1])
-      reverse_to_other = (-1 * to_other[0], -1 * to_other[1])
       target_a = (location[0] + to_other[0], location[1] + to_other[1])
-      target_b = (other[0] + reverse_to_other[0], other[1] + reverse_to_other[1])
       while (is_in_grid(target_a)):
         antinodes[target_a] = True
         target_a = (target_a[0] + to_other[0], target_a[1] + to_other[1])
+      target_b = (other[0] - to_other[0], other[1] - to_other[1])
       while (is_in_grid(target_b)):
         antinodes[target_b] = True
-        target_b = (target_b[0] + reverse_to_other[0], target_b[1] + reverse_to_other[1])
+        target_b = (target_b[0] - to_other[0], target_b[1] - to_other[1])
 
 print(len(antinodes))
